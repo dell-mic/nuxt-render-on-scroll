7b248d81ab30747f0878d38290449af38c0967c4
Michael Dell <michae.dell@live.de>
2020-11-14 17:10:20 +0100

    First public release

---
+++ dev/serve.vue
@@ -0,0 +1,45 @@
+<script>
+import Vue from "vue";
+import RenderOnScroll from "@/render-on-scroll.vue";
+
+export default Vue.extend({
+  name: "ServeDev",
+  components: {
+    RenderOnScroll,
+  },
+  data: function() {
+    return {
+      contents: [...Array(50).keys()].map((i) => ({
+        text: "sample content #" + i,
+      })),
+    };
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <render-on-scroll height="100px" v-for="item in contents" :key="item.text">
+      <div class="sampleContent">
+        {{ item.text }}
+      </div>
+    </render-on-scroll>
+  </div>
+</template>
+
+<style>
+.sampleContent {
+  height: 100px;
+  animation: fadein 3s;
+  text-align: center;
+}
+
+@keyframes fadein {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
---
+++ src/render-on-scroll.vue
@@ -0,0 +1,79 @@
+<template>
+  <div :style="styles">
+    <template v-if="render">
+      <slot />
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RenderOnScroll',
+  props: {
+    height: {
+      type: String,
+      default: '',
+    },
+    offsetY: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      render: false,
+    }
+  },
+  computed: {
+    styles() {
+      if (!this.render && this.height) {
+        return 'height: ' + this.height
+      } else {
+        return ''
+      }
+    },
+  },
+  mounted() {
+    if (this.isInViewport()) {
+      this.render = true
+    } else {
+      this.addListner()
+    }
+  },
+  methods: {
+    isInViewport() {
+      if (!this.$el || typeof window === 'undefined') return false
+
+      const scroll = window.scrollY || window.pageYOffset
+      const boundsTop = this.$el.getBoundingClientRect().top + scroll
+
+      const viewport = {
+        top: scroll,
+        bottom: scroll + window.innerHeight,
+      }
+
+      const bounds = {
+        top: boundsTop - this.offsetY,
+        bottom: boundsTop + this.$el.offsetHeight - this.offsetY,
+      }
+
+      const topInViewPort =
+        bounds.top <= viewport.bottom && bounds.top >= viewport.top
+      const bottomInViewPort =
+        bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom
+
+      return topInViewPort || bottomInViewPort
+    },
+    addListner() {
+      const interval = setInterval(() => {
+        if (this.isInViewport()) {
+          this.render = true
+          clearInterval(interval)
+        }
+      }, 200)
+    },
+  },
+}
+</script>
+
+<style></style>
