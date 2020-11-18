40397fdcdf39d2d5be28af6b41872e498edd279b
Michael Dell <1016675+dell-mic@users.noreply.github.com>
2020-11-18 18:01:40 +0100

    Update gitignore & check-in dist folder

---
+++ dist/render-on-scroll.ssr.js
@@ -0,0 +1,216 @@
+'use strict';Object.defineProperty(exports,'__esModule',{value:true});//
+//
+//
+//
+//
+//
+//
+//
+var script = {
+  name: 'RenderOnScroll',
+  props: {
+    height: {
+      type: String,
+      default: ''
+    },
+    offsetY: {
+      type: Number,
+      default: 0
+    }
+  },
+  data: function data() {
+    return {
+      render: false
+    };
+  },
+  computed: {
+    styles: function styles() {
+      if (!this.render && this.height) {
+        return 'height: ' + this.height;
+      } else {
+        return '';
+      }
+    }
+  },
+  mounted: function mounted() {
+    if (this.isInViewport()) {
+      this.render = true;
+    } else {
+      this.addListner();
+    }
+  },
+  methods: {
+    isInViewport: function isInViewport() {
+      if (!this.$el || typeof window === 'undefined') return false;
+      var scroll = window.scrollY || window.pageYOffset;
+      var boundsTop = this.$el.getBoundingClientRect().top + scroll;
+      var viewport = {
+        top: scroll,
+        bottom: scroll + window.innerHeight
+      };
+      var bounds = {
+        top: boundsTop - this.offsetY,
+        bottom: boundsTop + this.$el.offsetHeight - this.offsetY
+      };
+      var topInViewPort = bounds.top <= viewport.bottom && bounds.top >= viewport.top;
+      var bottomInViewPort = bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom;
+      return topInViewPort || bottomInViewPort;
+    },
+    addListner: function addListner() {
+      var _this = this;
+
+      var interval = setInterval(function () {
+        if (_this.isInViewport()) {
+          _this.render = true;
+          clearInterval(interval);
+        }
+      }, 200);
+    }
+  }
+};function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+    if (typeof shadowMode !== 'boolean') {
+        createInjectorSSR = createInjector;
+        createInjector = shadowMode;
+        shadowMode = false;
+    }
+    // Vue.extend constructor export interop.
+    const options = typeof script === 'function' ? script.options : script;
+    // render functions
+    if (template && template.render) {
+        options.render = template.render;
+        options.staticRenderFns = template.staticRenderFns;
+        options._compiled = true;
+        // functional template
+        if (isFunctionalTemplate) {
+            options.functional = true;
+        }
+    }
+    // scopedId
+    if (scopeId) {
+        options._scopeId = scopeId;
+    }
+    let hook;
+    if (moduleIdentifier) {
+        // server build
+        hook = function (context) {
+            // 2.3 injection
+            context =
+                context || // cached call
+                    (this.$vnode && this.$vnode.ssrContext) || // stateful
+                    (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+            // 2.2 with runInNewContext: true
+            if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+                context = __VUE_SSR_CONTEXT__;
+            }
+            // inject component styles
+            if (style) {
+                style.call(this, createInjectorSSR(context));
+            }
+            // register component module identifier for async chunk inference
+            if (context && context._registeredComponents) {
+                context._registeredComponents.add(moduleIdentifier);
+            }
+        };
+        // used by ssr in case component is cached and beforeCreate
+        // never gets called
+        options._ssrRegister = hook;
+    }
+    else if (style) {
+        hook = shadowMode
+            ? function (context) {
+                style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
+            }
+            : function (context) {
+                style.call(this, createInjector(context));
+            };
+    }
+    if (hook) {
+        if (options.functional) {
+            // register for functional component in vue file
+            const originalRender = options.render;
+            options.render = function renderWithStyleInjection(h, context) {
+                hook.call(context);
+                return originalRender(h, context);
+            };
+        }
+        else {
+            // inject component registration as beforeCreate hook
+            const existing = options.beforeCreate;
+            options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+        }
+    }
+    return script;
+}/* script */
+var __vue_script__ = script;
+/* template */
+
+var __vue_render__ = function __vue_render__() {
+  var _vm = this;
+
+  var _h = _vm.$createElement;
+
+  var _c = _vm._self._c || _h;
+
+  return _c('div', {
+    style: _vm.styles
+  }, [_vm.render ? [_vm._t("default")] : _vm._e()], 2);
+};
+
+var __vue_staticRenderFns__ = [];
+/* style */
+
+var __vue_inject_styles__ = undefined;
+/* scoped */
+
+var __vue_scope_id__ = undefined;
+/* module identifier */
+
+var __vue_module_identifier__ = "data-v-8199a2c2";
+/* functional template */
+
+var __vue_is_functional_template__ = false;
+/* style inject */
+
+/* style inject SSR */
+
+/* style inject shadow dom */
+
+var __vue_component__ = /*#__PURE__*/normalizeComponent({
+  render: __vue_render__,
+  staticRenderFns: __vue_staticRenderFns__
+}, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, false, undefined, undefined, undefined);// Import vue component
+
+var install = function installRenderOnScroll(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component('RenderOnScroll', __vue_component__);
+}; // Create module definition for Vue.use()
+
+
+var plugin = {
+  install: install
+}; // To auto-install on non-es builds, when vue is found
+// eslint-disable-next-line no-redeclare
+
+/* global window, global */
+
+{
+  var GlobalVue = null;
+
+  if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+  } else if (typeof global !== 'undefined') {
+    GlobalVue = global.Vue;
+  }
+
+  if (GlobalVue) {
+    GlobalVue.use(plugin);
+  }
+} // Inject install function into component - allows component
+// to be registered via Vue.use() as well as Vue.component()
+
+
+__vue_component__.install = install; // Export component by default
+// also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
+// export const RollupDemoDirective = component;
+exports.default=__vue_component__;
